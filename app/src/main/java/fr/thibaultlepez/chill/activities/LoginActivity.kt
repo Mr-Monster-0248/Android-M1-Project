da32f6a5dc5cb7dc62f17e557706038f65502aff
Ruben Nabet <rubenabet@gmail.com>
2021-03-17 10:09:25 +0100

    feat: link new SessionsListActivity in LoginActivity

--- app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
+++ app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
@@ -1,12 +1,22 @@
 package fr.thibaultlepez.chill.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import fr.thibaultlepez.chill.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        val loginButton = findViewById<Button>(R.id.login_btn);
+        loginButton?.setOnClickListener() {
+            // TODO: HANDLE AUTH
+            startActivity(Intent(this@LoginActivity, SessionsListActivity::class.java))
+            finish()
+        }
     }
 }
