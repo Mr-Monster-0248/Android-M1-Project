b53a45a83b7058046f1284c156114dc4fc559a48
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-17 09:50:10 +0100

    fix(login): fix login register btn

--- app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
+++ app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
@@ -1,12 +1,19 @@
 package fr.thibaultlepez.chill.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import fr.thibaultlepez.chill.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun goToRegisterActivity(view: View) {
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
