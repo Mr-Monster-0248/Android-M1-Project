0d5f273e448f9ff88ede08c21aea1d7d49eae61c
Ruben Nabet <rubenabet@gmail.com>
2021-03-17 10:23:30 +0100

    wip: start UsersListActivity

---
+++ app/src/main/java/fr/thibaultlepez/chill/activities/UsersListActivity.kt
@@ -0,0 +1,11 @@
+package fr.thibaultlepez.chill.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UsersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users_list)
+    }
+}
