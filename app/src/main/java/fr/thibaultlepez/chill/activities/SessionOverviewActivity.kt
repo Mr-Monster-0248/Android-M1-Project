a9fd710372e6dbbd330cf16691a3a3353326c482
Ruben Nabet <rubenabet@gmail.com>
2021-03-17 10:59:08 +0100

    refactor: rename UsersListActivity to SessionOverviewActivity and add edit button

--- app/src/main/java/fr/thibaultlepez/chill/activities/SessionOverviewActivity.kt
+++ app/src/main/java/fr/thibaultlepez/chill/activities/SessionOverviewActivity.kt
@@ -5,14 +5,22 @@ import android.os.Bundle
 import android.view.View
 import fr.thibaultlepez.chill.R
 
-class UsersListActivity : AppCompatActivity() {
+class SessionOverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_users_list)
+        setContentView(R.layout.activity_session_overview)
     }
 
 
+    // TODO: conditionally display EDIT button (only for session owner)
+
+
     fun goToSessionActivity(view: View) {
+        // TODO
+    }
+
+
+    fun editSession(view: View) {
         // TODO
     }
 
