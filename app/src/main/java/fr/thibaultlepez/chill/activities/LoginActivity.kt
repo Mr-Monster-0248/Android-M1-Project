00c5ec3979d01907cea5e375c18f1113e88b8b48
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-18 10:42:37 +0100

    feat: implement Login to the login activity

--- app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
+++ app/src/main/java/fr/thibaultlepez/chill/activities/LoginActivity.kt
@@ -3,24 +3,44 @@ package fr.thibaultlepez.chill.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 import fr.thibaultlepez.chill.R
+import fr.thibaultlepez.chill.services.loginWithEmailAndPassword
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+    private lateinit var loginEmail: TextInputEditText
+    private lateinit var loginPassword: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-    }
 
-
-    fun goToSessionListActivity(view: View) {
-        val intent = Intent(this@LoginActivity, SessionsListActivity::class.java)
-        startActivity(intent)
+        loginEmail = findViewById(R.id.login_email)
+        loginPassword = findViewById(R.id.login_password)
     }
 
     fun goToRegisterActivity(view: View) {
         val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    fun loginUser(view: View) {
+        try {
+            loginWithEmailAndPassword(loginEmail.text.toString(), loginPassword.text.toString()) {
+                goToSessionListActivity()
+            }
+        } catch (err: Error) {
+            Log.e("CHILL", "Error while logging user")
+        }
+    }
+
+
+    private fun goToSessionListActivity() {
+        val intent = Intent(this@LoginActivity, SessionsListActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
