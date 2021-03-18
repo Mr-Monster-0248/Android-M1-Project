80d0247031fc3b611eddcfb58ca08bad900d96f0
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-18 10:41:22 +0100

    feat: add the login function

--- app/src/main/java/fr/thibaultlepez/chill/services/Auth.kt
+++ app/src/main/java/fr/thibaultlepez/chill/services/Auth.kt
@@ -20,3 +20,20 @@ fun registerWithEmailAndPassword(email: String, password: String, success: (Fire
             }
         }
 }
+
+fun loginWithEmailAndPassword(email: String, password: String, success: (FirebaseUser) -> Unit ) {
+    val auth = FirebaseAuth.getInstance()
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("CHILL", "Log a user with success")
+
+                val user = auth.currentUser
+                if (user != null) success(user)
+            } else {
+                Log.w("CHILL", "Error while logging user", task.exception)
+                throw Error(task.exception)
+            }
+        }
+}
+
