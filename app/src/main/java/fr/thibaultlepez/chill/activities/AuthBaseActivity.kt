564badb6f943c848582d766dc12c6769c7b542b3
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-18 18:56:33 +0100

    feat: rename Base activity

---
+++ app/src/main/java/fr/thibaultlepez/chill/activities/AuthBaseActivity.kt
@@ -0,0 +1,48 @@
+package fr.thibaultlepez.chill.activities
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import fr.thibaultlepez.chill.R
+
+open class AuthBaseActivity : AppCompatActivity() {
+    private lateinit var progressDialog: Dialog
+
+    fun showSnackBar(message: String, error: Boolean = false) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (error) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@AuthBaseActivity,
+                    R.color.red_bittersweet
+                )
+            )
+        } else {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@AuthBaseActivity,
+                    R.color.green_aquamarine
+                )
+            )
+        }
+
+        snackBar.show()
+    }
+
+    fun showProgressDialog() {
+        progressDialog = Dialog(this)
+        progressDialog.setContentView(R.layout.dialog_progress)
+        progressDialog.setCancelable(false)
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        progressDialog.show()
+    }
+
+    fun closeProgressDialog() {
+        progressDialog.dismiss()
+    }
+}
