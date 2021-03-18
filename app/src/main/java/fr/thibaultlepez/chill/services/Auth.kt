5b6078f93a6111dbc44f1331f821f5bfcdb4cee7
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-18 10:39:54 +0100

    feat: add the register function

---
+++ app/src/main/java/fr/thibaultlepez/chill/services/Auth.kt
@@ -0,0 +1,22 @@
+package fr.thibaultlepez.chill.services
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.lang.Error
+
+fun registerWithEmailAndPassword(email: String, password: String, success: (FirebaseUser) -> Unit ) {
+    val auth = FirebaseAuth.getInstance()
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("CHILL", "Create a user with success")
+
+                val user = auth.currentUser
+                if (user != null) success(user)
+            } else {
+                Log.w("CHILL", "Error while creating user", task.exception)
+                throw Error(task.exception)
+            }
+        }
+}
