d4a9fcd230b7cef383b5eadfe663aca5f0b7bbac
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-20 18:38:31 +0100

    feat: add function to get session from firestore

--- app/src/main/java/fr/thibaultlepez/chill/services/Firestore.kt
+++ app/src/main/java/fr/thibaultlepez/chill/services/Firestore.kt
@@ -2,6 +2,7 @@ package fr.thibaultlepez.chill.services
 
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import fr.thibaultlepez.chill.models.FireSession
 import fr.thibaultlepez.chill.models.FireUser
 import fr.thibaultlepez.chill.models.User
 import fr.thibaultlepez.chill.utils.DbConstants
@@ -18,8 +19,24 @@ fun getUserFromDb(userId: String, callback: (FireUser) -> Unit) {
             if (user != null) callback(user)
         }
         .addOnFailureListener {
+            Log.e("CHILL/fail on DB", it.stackTraceToString())
             throw it
         }
+}
 
+fun getSessionFromDb(sessionId: String, callback: (FireSession) -> Unit) {
+    val db = FirebaseFirestore.getInstance()
+
+    db.collection(DbConstants.SESSIONS)
+        .document(sessionId)
+        .get()
+        .addOnSuccessListener {
+            val session = it.toObject(FireSession::class.java)
+            if (session != null ) callback(session)
+        }
+        .addOnFailureListener {
+            Log.e("CHILL/fail on DB", it.stackTraceToString())
+            throw it
+        }
 }
 
