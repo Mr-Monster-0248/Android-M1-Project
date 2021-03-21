1d271e985aee8c6986fde242c3075963b4fbe50c
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-21 16:13:19 +0100

    feat: Add link between list and overview

--- app/src/main/java/fr/thibaultlepez/chill/adapters/SessionListAdapter.kt
+++ app/src/main/java/fr/thibaultlepez/chill/adapters/SessionListAdapter.kt
@@ -2,14 +2,17 @@ package fr.thibaultlepez.chill.adapters
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import fr.thibaultlepez.chill.R
+import fr.thibaultlepez.chill.activities.SessionOverviewActivity
 import fr.thibaultlepez.chill.models.FireSession
 
 class SessionListAdapter(
@@ -17,9 +20,11 @@ class SessionListAdapter(
     private val list: ArrayList<FireSession>
 ) : RecyclerView.Adapter<SessionListAdapter.ViewHolder>() {
 
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.findViewById(R.id.card_session_title)
         val description: TextView = view.findViewById(R.id.card_session_description)
+        val card: CardView = view.findViewById(R.id.card_session_card)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -31,13 +36,14 @@ class SessionListAdapter(
         holder.title.text = list[position].name
         holder.description.text = getOwnerName(list[position])
 
-        holder.itemView.setOnClickListener {
-            val intent = Intent()
+        holder.card.setOnClickListener {
+            val intent = Intent(context, SessionOverviewActivity::class.java)
+            intent.putExtra("sessionId", list[position].id)
+            context.startActivity(intent)
         }
     }
 
     override fun getItemCount() = list.size
-
 
     private fun getOwnerName(elem: FireSession): String {
         val owner = elem.users.find { it.id == elem.ownerId }
