65d2f7776e18cd3ce87f3370db1def6b822b57cf
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-21 01:26:32 +0100

    feat: adapte sessions list to recycler view

--- app/src/main/java/fr/thibaultlepez/chill/activities/SessionsListActivity.kt
+++ app/src/main/java/fr/thibaultlepez/chill/activities/SessionsListActivity.kt
@@ -4,18 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import fr.thibaultlepez.chill.R
+import fr.thibaultlepez.chill.adapters.SessionListAdapter
 import fr.thibaultlepez.chill.store.State
 import kotlinx.coroutines.*
 
 class SessionsListActivity : BaseActivity() {
+    lateinit var sessionList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sessions_list)
 
-        Log.i("CHILL", "Il se passe qqchose")
-        Log.i("CHILL", State.toString())
+        sessionList = findViewById(R.id.sessions_list_list_view)
 
         val user = FirebaseAuth.getInstance().currentUser
 
@@ -23,11 +30,17 @@ class SessionsListActivity : BaseActivity() {
             State.initState(user!!)
 
             withContext(Dispatchers.Main) {
-                // TODO: update UI
+                displayList()
             }
 
             Log.i("CHILL", State.toString())
         }
+    }
+
+    private fun displayList() {
+        sessionList.layoutManager = LinearLayoutManager(this, GridLayoutManager.VERTICAL, false)
+        sessionList.adapter =  SessionListAdapter(this, State.sessions)
+
     }
 
     fun goToJoinActivity(view: View) {
@@ -35,6 +48,4 @@ class SessionsListActivity : BaseActivity() {
         startActivity(intent)
     }
 
-
-    // TODO: link session to its activity
 }
