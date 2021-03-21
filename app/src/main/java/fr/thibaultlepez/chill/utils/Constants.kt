e3ec809c14cc624ee08f1d2a8b89b188db82a7f3
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-03-21 12:54:23 +0100

    feat: add join session feature

--- app/src/main/java/fr/thibaultlepez/chill/utils/Constants.kt
+++ app/src/main/java/fr/thibaultlepez/chill/utils/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val WOULD_NOT_WATCH_SCORE = -1
 
     const val TMDB_API_KEY = "bd8a26fab1a21437628c0ad96c66a13e"
+
+    const val ADD_USER_TO_SESSION_FUNC = "addUserToSession"
 }
